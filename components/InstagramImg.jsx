8ec25d6ca3cbbd404d0bafcf2aa188e639ca3853
Rodrigo Estabel Boquer <rodrigo-boquer@IPESAUDE.INTRA.RS.GOV.BR>
2023-01-16 12:50:22 -0300

    Finalizando um site com Next JS, parecendo um portifolio de fotografias!

---
+++ components/InstagramImg.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstagramImg = () => {
+    return (
+        <div>
+            <Image />
+        </div>
+    )
+}
+
+export default InstagramImg
---
+++ components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1673207559693-705d2a40d4cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1673294245958-89d2c1a9a36d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1673269595891-b92b2fa6edd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1673286900865-ab88ead9285b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
+    },
+];
